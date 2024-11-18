b0215afdf8da40cf91c6d22b948d99340942cc1e
Kirill <kiria12312@mail.ru>
2024-11-18 07:12:02 +0300

    fix background image sizes

--- screens/home-screen.tsx
+++ screens/home-screen.tsx
@@ -46,7 +46,8 @@ export default function HomeScreene() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
+    width: '100%',
     resizeMode: "cover",
   },
 });
